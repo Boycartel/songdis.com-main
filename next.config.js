541e1be1d97447ad875741a5bb94597bcbbed191
Tajudee Faruk Ayoola <89199160+Boycartel@users.noreply.github.com>
2025-01-10 08:05:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+        appDir: true, // This enables the `app` directory routing
+    },
+};
+
+module.exports = nextConfig;
