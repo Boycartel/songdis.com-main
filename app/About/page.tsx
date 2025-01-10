d31031c5a9de3c32e87de655e4bfaf748989a9d2
Tajudee Faruk Ayoola <89199160+Boycartel@users.noreply.github.com>
2025-01-10 07:50:46 +0100

    Update page.tsx

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,1 +1,19 @@
+"use client";
+import MainLayout from './layouts/main-layout';
+import About from './components/About';
 
+export default function Home() {
+  return (
+    <MainLayout>
+      <main>
+        <div className="w-full">
+          <div className="relative w-full px-3 pb-20 pt-36 sm:pt-40 banner-image">
+            <div className="relative z-10 text-center">
+              <About />
+            </div>
+          </div>
+        </div>
+      </main>
+    </MainLayout>
+  )
+}
