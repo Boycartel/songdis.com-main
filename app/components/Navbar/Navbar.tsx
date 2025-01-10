93143d4407651ec2547dddc0a30311a5e7029e7e
Tajudee Faruk Ayoola <89199160+Boycartel@users.noreply.github.com>
2025-01-10 07:54:14 +0100

    Update Navbar.tsx

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import Drawer from "./Drawer";
 import Drawerdata from "./Drawerdata";
 
-
 interface NavigationItem {
     name: string;
     href: string;
@@ -19,83 +18,92 @@ const Navbar = () => {
     const [isRegisterOpen, setIsRegisterOpen] = React.useState(false);
 
     const navigation: NavigationItem[] = [
-        { 
-            name: 'Distribute', 
-            href: '/', 
-            current: true, 
-            onClick: () => setIsRegisterOpen(true)
+        {
+            name: 'Distribute',
+            href: '/',
+            current: true,
+            onClick: () => setIsRegisterOpen(true),
         },
-        { name: 'Services', href: 'services', current: false },
-        { name: 'About', href: 'about', current: false },
-        { name: 'Plan', href: 'plan', current: false },
+        { name: 'Services', href: '/services', current: false },
+        { name: 'About', href: '/about', current: false },
+        { name: 'Plan', href: '/plan', current: false },
     ];
 
-    function classNames(...classes: string[]) {
-        return classes.filter(Boolean).join(' ');
-    }
+    const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ');
 
     return (
         <Disclosure as="nav" className="fixed top-0 z-50 w-full bg-white shadow navbar">
             <>
                 <div className="px-6 mx-auto max-w-7xl lg:py-4 lg:px-8">
                     <div className="relative flex items-center justify-between h-20">
+                        {/* Logo */}
                         <div className="flex items-center flex-1 sm:items-stretch sm:justify-start">
                             <div className="flex items-center flex-shrink-0">
                                 <img
                                     className="block lg:hidden"
-                                    src={'/assets/logo/logo.png'}
+                                    src="/assets/logo/logo.png"
                                     alt="dsign-logo"
-                                    style={{ height: '140px', width: 'auto' }} 
+                                    style={{ height: '140px', width: 'auto' }}
                                 />
                                 <img
                                     className="hidden lg:block"
-                                    src={'/assets/logo/logo.png'}
+                                    src="/assets/logo/logo.png"
                                     alt="dsign-logo"
-                                    style={{ height: '250px', width: 'auto' }} 
+                                    style={{ height: '250px', width: 'auto' }}
                                 />
                             </div>
-                            <div className="hidden m-auto lg:block">
-                                <div className="flex space-x-4">
-                                    {navigation.map((item) => (
-                                        <Link
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'text-black hover:opacity-100' : 'hover:text-black hover:opacity-100',
-                                                'px-3 py-4 text-lg font-normal opacity-75 space-links'
-                                            )}
-                                            onClick={(e) => {
-                                                if (item.onClick) {
-                                                    e.preventDefault();
-                                                    item.onClick();
-                                                }
-                                            }}
-                                            aria-current={item.href ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </Link>
-                                    ))}
-                                </div>
+                        </div>
+
+                        {/* Desktop Navigation */}
+                        <div className="hidden m-auto lg:block">
+                            <div className="flex space-x-4">
+                                {navigation.map((item) => (
+                                    <Link
+                                        key={item.name}
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'text-black hover:opacity-100' : 'hover:text-black hover:opacity-100',
+                                            'px-3 py-4 text-lg font-normal opacity-75 space-links'
+                                        )}
+                                        onClick={(e) => {
+                                            if (item.onClick) {
+                                                e.preventDefault();
+                                                item.onClick();
+                                            }
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ))}
                             </div>
                         </div>
 
+                        {/* Desktop Auth Links */}
                         <div className="hidden space-x-4 lg:flex">
-                            <Link 
-                             className="px-4 py-2 text-lg font-medium text-black hover:bg-gray-200" 
-                            href="/auth/signin">Sign In</Link>
-                            <Link 
-                             className="px-4 py-2 text-lg font-medium text-white bg-red-600 hover:bg-red-700 border-lightgrey leafbutton"
-                            href="/auth/signup">Sign Up</Link>
+                            <Link
+                                className="px-4 py-2 text-lg font-medium text-black hover:bg-gray-200"
+                                href="/auth/signin"
+                            >
+                                Sign In
+                            </Link>
+                            <Link
+                                className="px-4 py-2 text-lg font-medium text-white bg-red-600 hover:bg-red-700 border-lightgrey leafbutton"
+                                href="/auth/signup"
+                            >
+                                Sign Up
+                            </Link>
                         </div>
 
-                    
-                     
-                        {/* DRAWER ICON FOR MOBILE */}
-                        <div className='block lg:hidden'>
-                            <Bars3Icon className="block w-6 h-6" aria-hidden="true" onClick={() => setIsDrawerOpen(true)} />
+                        {/* Drawer Icon for Mobile */}
+                        <div className="block lg:hidden">
+                            <Bars3Icon
+                                className="block w-6 h-6"
+                                aria-hidden="true"
+                                onClick={() => setIsDrawerOpen(true)}
+                            />
                         </div>
 
-                        {/* DRAWER LINKS DATA */}
+                        {/* Drawer Links for Mobile */}
                         <Drawer isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen}>
                             <Drawerdata
                                 openSignIn={() => setIsSignInOpen(true)}
@@ -107,6 +115,6 @@ const Navbar = () => {
             </>
         </Disclosure>
     );
-}
+};
 
 export default Navbar;
